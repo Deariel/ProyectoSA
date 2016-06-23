67a300198555e89f9af5880c90477d97c8ff6a26
Deariel <dennisarielper@gmail.com>
2016-06-23 16:53:57 -0600

    Metodo CrearContribuyente

---
+++ EntidadSat/App_Code/Entidad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Entidad
+/// </summary>
+namespace RequestContribuyente
+{
+    public class CrearContribuyente
+    {
+        public string Nombre { get; set; }
+    }
+}
+namespace ResponseContribuyente
+{
+    public class CrearContribuyente
+    {
+        public int Estado { get; set; }
+        public string MensajeRespuesta { get; set; }
+        public string NIT { get; set; }
+    }
+}
--- EntidadSat/App_Code/Service.cs
+++ EntidadSat/App_Code/Service.cs
@@ -18,8 +18,30 @@ public class Service : System.Web.Services.WebService
     }
 
     [WebMethod]
-    public string HelloWorld() {
-        return "Hello World";
+    public string RequestCrearContribuyente(string xmlContribuyente) {
+        RequestContribuyente.CrearContribuyente contribuyente = new RequestContribuyente.CrearContribuyente ();
+        contribuyente = (RequestContribuyente.CrearContribuyente)ConvertirXML.XmlDeserealizar(xmlContribuyente, typeof(RequestContribuyente.CrearContribuyente));
+        GestionSat.Class1 crearContribuyente = new GestionSat.Class1();
+        string nitContribuyente = "7821998-1";//contribuyente();
+        ResponseContribuyente.CrearContribuyente xml = new ResponseContribuyente.CrearContribuyente();
+        if (!nitContribuyente.Equals("-1"))
+        {
+            xml.Estado = 0;
+            xml.MensajeRespuesta = "Transaccion Exitosa";
+            xml.NIT = nitContribuyente;
+        }
+        else
+        {
+            xml.Estado = 1;
+            xml.MensajeRespuesta = "Transaccion Fallida. Intentelo de nuevo mas tarde.";
+        }
+
+        //Guardar Transaccion en BitacoraSat
+        BitacoraSat.Class1 transaccion = new BitacoraSat.Class1();
+        String Respuesta = ConvertirXML.XmlSerializar(xml);
+        transaccion.InsertarTransaccion(Respuesta, xmlContribuyente, "", "RequestCrearContribuyente");
+
+        return Respuesta;
     }
     
 }
